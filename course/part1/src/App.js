32b7265c726d3e66f6ef1e3bbdb1d71570b75b41
lo0b0o <tctsui@outlook.com>
2021-08-30 14:16:07 +0800

    finish lec. part1d

--- course/part1/src/App.js
+++ course/part1/src/App.js
@@ -1,33 +1,34 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
-
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text='plus' />
-      <Button onClick={decreaseByOne} text='minus' />
-      <Button onClick={setToZero} text='reset' />
-    </div >
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+    </div>
   )
 }
 
-const Display = ({ counter }) => (
-  <div>
-    {counter}
-  </div>
-)
-
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
+const History = ({allClicks}) => {
+  return (
+    !allClicks.length 
+      ? <div>the app is used by pressing the buttons</div>
+      : <div>button press history:{allClicks.join(' ')}</div>)
+}
 
 
 export default App
